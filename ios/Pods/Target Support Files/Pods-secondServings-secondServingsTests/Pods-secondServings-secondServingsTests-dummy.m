fc08ad71620b976b6a5eca6402a66e1243d40e10
myra <myraramdenbourg@gmail.com>
2020-08-09 18:14:45 -0500

    init react native

---
+++ ios/Pods/Target Support Files/Pods-secondServings-secondServingsTests/Pods-secondServings-secondServingsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_secondServings_secondServingsTests : NSObject
+@end
+@implementation PodsDummy_Pods_secondServings_secondServingsTests
+@end
